7e663b19fcc10ae7a5098b44c872956f294f685e
mishtusorous <mishtusorous@gmail.com>
2016-03-25 21:25:27 +0530

    Starting with reflection, in case I can change the config world.

---
+++ src/main/java/fun/n/learn/reflection/Reflection001.java
@@ -0,0 +1,15 @@
+package fun.n.learn.reflection;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Reflection001 {
+	private final static Logger logger = LoggerFactory
+			.getLogger("fun.n.learn.reflection.Reflection001");
+
+	// http://tutorials.jenkov.com/java-reflection/index.html
+
+	public static void main(String[] args) {
+		logger.debug("Hello world.");
+	}
+}
