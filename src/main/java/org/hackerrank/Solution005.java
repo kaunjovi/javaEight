27cae5b6d5ed35aabfa3b7d9f78c0813e9d0137a
kaunjovi <kaunjovi@gmail.com>
2016-04-17 11:40:35 +0530

    Still testing waters.

---
+++ src/main/java/org/hackerrank/Solution005.java
@@ -0,0 +1,20 @@
+package org.hackerrank;
+
+import java.util.Scanner;
+
+public class Solution005 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int i1 = sc.nextInt();
+		int i2 = sc.nextInt();
+		int i3 = sc.nextInt();
+
+		System.out.println(i1);
+		System.out.println(i2);
+		System.out.println(i3);
+	}
+
+}
