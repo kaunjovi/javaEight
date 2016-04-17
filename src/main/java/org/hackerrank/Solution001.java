9c67bf607bdf55264a64ec3f236c5ca9d61935ab
kaunjovi <kaunjovi@gmail.com>
2016-04-17 11:29:52 +0530

    Let's get started.

---
+++ src/main/java/org/hackerrank/Solution001.java
@@ -0,0 +1,13 @@
+package org.hackerrank;
+
+//https://www.hackerrank.com/challenges/welcome-to-java/submissions/code/19436913
+
+public class Solution001 {
+
+	public static void main(String[] argv) {
+		// Write your solution here.
+		System.out.println("Hello World.");
+		System.out.println("Hello Java.");
+	}
+
+}
